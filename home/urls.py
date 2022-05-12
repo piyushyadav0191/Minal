155c96f99de074defdcf73d55f04aefc8d2ecbec
piyushyadav0191 <piyushyadav0191@gmail.com>
2022-05-12 21:34:20 +0530

    here we go again

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,9 @@ from home import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('contact', views.contact, name='contact'),
+    path('signup.html', views.signup, name='signup'),
+    path('login.html', views.login, name='login'),
+    path('calendar.html', views.calendar, name='calendar'),
+    path('contact.html', views.contact, name='contact'),
+   
 ]
--- home/views.py
+++ home/views.py
@@ -5,8 +5,14 @@ from django.shortcuts import render
 def home(request):
     return render(request,'home.html')
 
-def about(request):
-       return render(request,'about.html')
+def signup(request):
+       return render(request,'signup.html')
+
+def login(request):
+       return render(request,'login.html')
+
+def calendar(request):
+       return render(request,'calendar.html')
 
 def contact(request):
-    return render(request,'contact.html')
+       return render(request,'contact.html')
